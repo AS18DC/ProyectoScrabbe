63691aa5fc722d4e76d937c6851f5060ebbf69a0
mqrcoo <marcoa13310@gmail.com>
2025-01-14 21:05:31 -0400

    decorado de colores de menus de app juego y app gestion

--- src/main/java/appScrabble/MainJuegoApplication.java
+++ src/main/java/appScrabble/MainJuegoApplication.java
@@ -15,7 +15,7 @@ public class MainJuegoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainJuegoApplication.class.getResource("main-juego-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1860, 1000);
         stage.setTitle("Scrabble - Aplicacion de Juego");
         stage.setScene(scene);
         stage.show();
